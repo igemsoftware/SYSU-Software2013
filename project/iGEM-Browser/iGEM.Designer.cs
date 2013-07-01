f81950652fb22a006d0a062a33ee2f77b689a1da
guojiex <1036479561@qq.com>
2013-07-01 23:33:13 +0800

    add the browser folder
    Signed-off-by: guojiex <1036479561@qq.com>

---
+++ project/iGEM-Browser/iGEM.Designer.cs
@@ -0,0 +1,48 @@
+﻿namespace iGEM
+{
+    partial class iGEM
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // iGEM
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1045, 571);
+            this.Name = "iGEM";
+            this.Text = "iGEM-SYSU";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
---
+++ project/iGEM-Browser/iGEM.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using CefSharp.WinForms;
+using CefSharp;
+
+namespace iGEM
+{
+    public partial class iGEM : Form
+    {
+        WebView webView;
+        public iGEM()
+        {
+            InitializeComponent();
+            this.WindowState = FormWindowState.Maximized;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            BrowserSettings now = new BrowserSettings();
+            now.PageCacheDisabled = false;
+            now.LocalStorageDisabled = false;
+            now.DragDropDisabled = false;
+            now.JavaScriptDisabled = false;
+            webView = new WebView("http://127.0.0.1:5000", now);
+            //webView.PropertyChanged += new PropertyChangedEventHandler(webView_PropertyChanged);
+            webView.Dock = DockStyle.Fill;
+            this.Controls.Add(webView);
+        }
+
+
+        
+        /**
+         * Method About explorer history operation
+         */
+        void webView_PropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            webView = (WebView)sender;
+            switch (e.PropertyName)
+            {
+                case "IsBrowserInitialized":
+                    //webView.IsBrowserInitialized
+                    break;
+                case "Title":
+                    //webView.Title
+                    break;
+                case "Address":
+                    //webView.Address
+                    break;
+                case "CanGoBack":
+                    //webView.CanGoBack
+                    break;
+                case "CanGoForward":
+                    //webView.CanGoForward;
+                    break;
+                case "IsLoading":
+                    //webView.IsLoading
+                    break;
+            }
+        }
+    }
+}
