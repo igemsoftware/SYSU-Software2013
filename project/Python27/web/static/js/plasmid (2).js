08ad4cdb7f6dca4b1f1e6515a2235be2587edb44
guojiex <1036479561@qq.com>
2013-07-02 01:52:14 +0800

    change the plasmid.js to make it relate to the data.json
    Signed-off-by: guojiex <1036479561@qq.com>

--- project/Python27/web/static/js/plasmid (2).js
+++ project/Python27/web/static/js/plasmid (2).js
@@ -1,16 +1,78 @@
 var data =  [];
+var size=0;
+var raw_data={
+    "DnaComponent": {
+        "description": "undefined",
+        "annotaions": [{"SequenceAnnotation": {"bioStart": "49",
+                    "subComponent": {
+                        "DnaComponent": {
+                            "displayId": "4932",
+                            "uri": "http: //partsregistry.org/Part: BBa_E1010",
+                            "type": "Coding",
+                            "description": "**highly**engineeredmutantofredfluorescentproteinfromDiscosomastriata(coral)",
+                            "name": "BBa_E1010"
+                        }
+                    },
+                    "uri": "http: //sbols.org/",
+                    "strand": "+",
+                    "bioEnd": "730"
+                }
+            },{
+                "SequenceAnnotation": {
+                    "bioStart": "736",
+                    "subComponent": {
+                        "DnaComponent": {
+                            "displayId": "144",
+                            "uri": "http: //partsregistry.org/Part: BBa_B0011",
+                            "type": "Terminator",
+                            "description": "LuxICDABEG(+/-)",
+                            "name": "BBa_B0011"
+                        }
+                    },
+                    "uri": "http: //sbols.org/",
+                    "strand": "+",
+                    "bioEnd": "782"
+                }
+            }
+        ],
+        "uri": "http: //sbol.org/",
+        "DnaSequence": {
+            "nucleotides": "GAATTCGCGGCCGCTTCTAGATGGCCGGCGAATTCGCGGCCGCTTCTAGatggcttcctccgaagacgttatcaaagagttcatgcgtttcaaagttcgtatggaaggttccgttaacggtcacgagttcgaaatcgaaggtgaaggtgaaggtcgtccgtacgaaggtacccagaccgctaaactgaaagttaccaaaggtggtccgctgccgttcgcttgggacatcctgtccccgcagttccagtacggttccaaagcttacgttaaacacccggctgacatcccggactacctgaaactgtccttcccggaaggtttcaaatgggaacgtgttatgaacttcgaagacggtggtgttgttaccgttacccaggactcctccctgcaagacggtgagttcatctacaaagttaaactgcgtggtaccaacttcccgtccgacggtccggttatgcagaaaaaaaccatgggttgggaagcttccaccgaacgtatgtacccggaagacggtgctctgaaaggtgaaatcaaaatgcgtctgaaactgaaagacggtggtcactacgacgctgaagttaaaaccacctacatggctaaaaaaccggttcagctgccgggtgcttacaaaaccgacatcaaactggacatcacctcccacaacgaagactacaccatcgttgaacagtacgaacgtgctgaaggtcgtcactccaccggtgcttaataaACCGGCagagaatataaaaagccagattattaatccggcttttttattatttACCGGTTAATACTAGTAGCGGCCGCTGCAG",
+            "uri": "http: //sbols.org/"
+        },
+        "displayId": "undefined",
+        "name": "undefined"
+    }
+};
+function turnRawDatatoData(raw)
+{
+	//var i=raw["DnaComponent"]["annotaions"].length;
+	var tempArray=[];
+	size=raw["DnaComponent"]["DnaSequence"]["nucleotides"].length;
+	for(i=0;i<raw["DnaComponent"]["annotaions"].length;i++)
+	{
+		tempArray[i]={start:raw["DnaComponent"]["annotaions"][i]["SequenceAnnotation"]["bioStart"]};
+		tempArray[i]["name"]=raw["DnaComponent"]["annotaions"][i]["SequenceAnnotation"]["subComponent"]["DnaComponent"]["name"];
+		tempArray[i]["end"]=raw["DnaComponent"]["annotaions"][i]["SequenceAnnotation"]["bioEnd"];
+		tempArray[i]["value"]=parseInt((tempArray[i]["end"]-tempArray[i]["start"])/size*100);
+	}	
+	console.log(tempArray);
+	console.log(size);
+	return tempArray;
+}
 function getData()//to get the real data of plasmid
 {
 	data = [
-		{name : 'HTML5&CSS3',value : 30,color:'#fedd74',rna:'ab1'},
-		{name : 'JavaScript',value : 25,color:'#82d8ef',rna:'ab2'},
-		{name : 'Java',value : 15,color:'#f76864',rna:'ab3'},
-		{name : 'XML',value : 20,color:'#80bd91',rna:'ab4e'},
-		{name : 'PhotoShop',value : 10,color:'#fd9fc1',rna:'ab5'}
+		{name : 'HTML5&CSS3',value : 6,color:'#f4f4f4',rna:'0 to 48'},
+		{name : 'JavaScript',value : 83,color:'#82d8ef',rna:'49 to 730'},
+		{name : 'Java',value : 1,color:'#f4f4f4',rna:'731 to 735'},
+		{name : 'XML',value : 6,color:'#80bd91',rna:'736 to 782'},
+		{name : 'PhotoShop',value : 4,color:'#f4f4f4',rna:'783 to 812'}
 	];
 }
 $(function(){
 	getData();
+	turnRawDatatoData(raw_data);
 	var chart = new iChart.Donut2D({
 		animation:true,
 		render : 'canvasDiv',//图表渲染的HTML DOM的id
@@ -23,6 +85,7 @@ $(function(){
 			shadow_color:'#b7b7b7',
 			color:'#6f6f6f'
 		},*/
+		//offset_angle: 270,
 		data: data,//图表的数据源
 		offsetx:-60,
 		shadow:true,
@@ -32,13 +95,14 @@ $(function(){
 			enable:true,
 			showType:'fixed',
 			animation:true,
-			fade_duration:700,
+			//fade_duration:700,
 			listeners:{
 				parseText:function(tip,name,value,text){
 					var str= name;						
 					for(i=0;i<data.length;i++){
 						if(data[i]["name"]==name){
 							str=str+"<br\/>"+data[i]["rna"];
+							break;
 						}
 					}
 					return str;
