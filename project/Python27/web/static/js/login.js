666c06dd86c7ca0fd49a02ccfcf781ffb0c8334f
rathinho <rathinho17@gmail.com>
2013-07-31 22:24:16 +0800

    new login view

--- project/Python27/web/static/js/login.js
+++ project/Python27/web/static/js/login.js
@@ -1,27 +1,63 @@
-
-$(document).ready(function () {
+$(document).ready(function() {
 	console.log(location.href);
 	if ("WebSocket" in window) {
-	  	ws = new WebSocket("ws://" + document.domain + ":5000/ws");
-	  	ws.onmessage = function (msg) {
-		    var message = JSON.parse(msg.data);		    
-		    if(message.result === "Password correct!") {
-		    	// redirect to "index.html"
-				alert("Welcome back! user: "+$("#username").attr('value'));
-				
-				window.location =location.href+"index";
-		    }else{
+		ws = new WebSocket("ws://" + document.domain + ":5000/ws");
+		ws.onmessage = function(msg) {
+			var message = JSON.parse(msg.data);
+			if (message.result === "Password correct!") {
+				// redirect to "index.html"
+				alert("Welcome back! user: " + $("#username").attr('value'));
+
+				window.location = location.href + "index";
+			} else {
 				alert(message.result);
 			}
 		};
-    };
+	};
 
-    // Bind send button to websocket
-    $("#btn-login").live("click", function() {
-    	var username = $("#username").attr('value');
-    	var password = $("#password").attr('value');
+	// Bind send button to websocket
+	$("#btn-login").live("click", function() {
+		var username = $("#username").attr('value');
+		var password = $("#password").attr('value');
 
-    	//ws.send(JSON.stringify({'request': 'get_part', 'table_name': 'part_list'}));
-      	ws.send(JSON.stringify({'request': 'userLogin','name': username, 'password': password}));
-    });    
+		//ws.send(JSON.stringify({'request': 'get_part', 'table_name': 'part_list'}));
+		ws.send(JSON.stringify({
+			'request': 'userLogin',
+			'name': username,
+			'password': password
+		}));
+	});
+
+	$("#btn-reset").live("click", function() {
+		$("#username").reset();
+		$("#password").reset();
+	});
+
+	$("#username").change(function() {
+		if ($(this).val()) {
+			$(this).css({
+				"background": "rgba(80, 80, 80, 0.3)"
+			});
+		} else {
+			// $(this).css({
+			// 	"background-image": "url(../static/img/user.png)",
+			// 	"background-position": "5px 5px",
+			// 	"background-repeat": "no-repeat"
+			// });
+		}
+	});
+
+	$("#password").change(function() {
+		if ($(this).val()) {
+			$(this).css({
+				"background": "rgba(80, 80, 80, 0.3)"
+			});
+		} else {
+			// $(this).css({
+			// 	"background-image": "url(../static/img/password.png)",
+			// 	"background-position": "5px 5px",
+			// 	"background-repeat": "no-repeat"
+			// });
+		}
+	});
 });
