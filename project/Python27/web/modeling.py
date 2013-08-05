bddcc14643f08caa23f7f46f55e34fcf368fe49d
guojiex <1036479561@qq.com>
2013-08-05 16:08:54 +0800

    Signed-off-by: guojiex <1036479561@qq.com>

--- project/Python27/web/modeling.py
+++ project/Python27/web/modeling.py
@@ -7,16 +7,20 @@ a set of methods that using json or the changing of class that related to json
 Copyright (C) 2013-2014 sysu-software. All Rights Reserved.
 '''
 import json
+import jsonUtil
 from database import SqliteDatabase
 class modeling:
 	def __init__(self,database,AValue,BValue):
 		self.db = database
 		self.__ProteinAValue=AValue
 		self.__ProteinBValue=BValue
-		closeA=self.__getBestExpressionValueRecord(self.__ProteinAValue)
-		print closeA
-		closeB=self.__getBestExpressionValueRecord(self.__ProteinBValue)
-		print closeB
+		self.__A=self.__getBestExpressionValueRecord(self.__ProteinAValue)
+		self.__APromoter= self.__getPromoterByExpressionValueRecord(self.__A)
+		self.__APlasmidBackbone= self.__getPlasmidBackboneByExpressionValueRecord(self.__A)
+		self.__B=self.__getBestExpressionValueRecord(self.__ProteinBValue)
+		self.__BPromoter= self.__getPromoterByExpressionValueRecord(self.__B)
+		self.__BPlasmidBackbone= self.__getPlasmidBackboneByExpressionValueRecord(self.__B)
+		print self.__BPromoter
 
 	def __getBestExpressionValueRecord(self,idealValue):
 		data= self.db.getExpressionValue()
@@ -27,6 +31,20 @@ class modeling:
 				minIndex=i
 				minDiff=abs(data[i]['ExpressionValue']-idealValue)
 		return data[minIndex]
+	
+	def __getPromoterByExpressionValueRecord(self,ExpressionValueRecord):
+		self.db._SqliteDatabase__cursor.execute('select * from promoter where Number="%s"' %(ExpressionValueRecord['Promoter']))		
+		jsonEncoded = jsonUtil.turnSelectionResultToJson(self.db._SqliteDatabase__cursor.description,self.db._SqliteDatabase__cursor.fetchall())
+		decodejson = json.loads(jsonEncoded)
+		return decodejson
+	
+	def __getPlasmidBackboneByExpressionValueRecord(self,ExpressionValueRecord):
+		self.db._SqliteDatabase__cursor.execute('select * from plasmid_backbone where Number="%s"' %(ExpressionValueRecord['PlasmidBackbone']))		
+		jsonEncoded = jsonUtil.turnSelectionResultToJson(self.db._SqliteDatabase__cursor.description,self.db._SqliteDatabase__cursor.fetchall())
+		decodejson = json.loads(jsonEncoded)
+		return decodejson
+	
+	def 
 
 if __name__=="__main__":
 	sql=SqliteDatabase()
