b0f9d22f77b5a6c6faed887fa251e1a63afe7f65
whyuan <519316166@qq.com>
2013-08-22 01:31:44 +0800

    修改genecircuit plamids排版

--- project/Python27/web/static/js/genecircuit.js
+++ project/Python27/web/static/js/genecircuit.js
@@ -5,7 +5,7 @@ $.fn.dashboard = function(options) {
 	return this.each(function() {
 		/* $(this).append("aaa"); */
 		$(this).append("<div class=\"dashboard-arrow\"></div>");
-		/* $(this).append("<div class=\"dashboard-value\">50%</div>"); */
+		$(this).append("50%");
 		/* console.log(this); */
 			/* $(this).css("font-size", "60px"); */
 		if(options.size) {
@@ -19,15 +19,41 @@ $.fn.dashboard = function(options) {
       percentage = 100;
     else if (percentage < 0)
       percentage = 0;
-    $(this).find(".dashboard-arrow").css('transform', 'rotate(' + ((1.8 * percentage) - 90) + 'deg)');
+		percentage = Math.floor(percentage);
+		$(this).append("<div class=\"dashboard-value\">" + (percentage.toString()) + "%</div>");
+    $(this).find(".dashboard-arrow").css('transform', 'rotate(' + ((1.8 * percentage) - 180) + 'deg)');
 	});
 }
-	
 })(jQuery);
+
+
 
 $(function(){
 	$(".dashboard").dashboard({
 		size: 40,
 		percentage: 20,
 	});
+
+	$(".sbol-components").sortable({
+		items: "li",
+		handle: "img, span",
+	});
+	$("#plasmids-view").sortable({
+		items: ".sbol",
+		handle: ".sbol-handle",
+	});
+	console.log($("#plasmids-view"));
+	console.log($(".dashboard"))
+	/* $(".plasmids").sortable({items:"ul"}); */
 });
+/*  */
+/* E.config({ */
+	/* baseUrl : './static/js/regulation/slider/js/' */
+/* }); */
+/* E.use('slider', function(){ */
+	/* var slider = new E.ui.Slider('.protein-slider', { */
+		/* min : -100, */
+		/* max : 100, */
+		/* axis : 'x', */
+	/* }); */
+/* }); */
