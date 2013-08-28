c45142901f7e6963150c7ff4d4a1c2706dce8e25
guojiex <1036479561@qq.com>
2013-08-28 15:08:54 +0800

    更改为xxtea加密算法，以及其javascript实现
    Signed-off-by: guojiex <1036479561@qq.com>

--- project/Python27/web/encrypt.py
+++ project/Python27/web/encrypt.py
@@ -1,58 +1,95 @@
 #coding=utf-8
-import hashlib
-import sys
-import base64
-import time
-m = hashlib.md5()
-
-def getMD5(str):
-	m.update(str)
-	return m.hexdigest()
-
-default_encoding = 'utf-8'
-if sys.getdefaultencoding() != default_encoding:
-    reload(sys)
-    sys.setdefaultencoding(default_encoding)
-
-class AzDG:
-	cipher = '0123456789'
-	charset = 'utf-8'	
-	def __init__(self, cipher = None):
-		if cipher != None:
-			self.cipher = cipher
-	def getCipher(self):
-		return self.cipher
-	def cipherEncode(self, sourceText):	
-		cipherHash = hashlib.md5(self.getCipher()).hexdigest()
-		cipherEncodeText = ''
-		for i in range(len(sourceText)):
-			cipherEncodeText = '%s%s' % (cipherEncodeText, chr(ord(sourceText[i]) ^ ord(cipherHash[i%32])))
-		return cipherEncodeText
-	def encode(self, sourceText, charset = 'utf-8'):
-		if charset != self.charset:
-			sourceText = sourceText.encode(charset)
-		noise = hashlib.md5('%s' % (time.time())).hexdigest()
-		encodeText = ''
-		for i in range(len(sourceText)):
-			encodeText = '%s%s%s' % (encodeText, noise[i%32], chr(ord(sourceText[i]) ^ ord(noise[i%32])))
-		return base64.b64encode(self.cipherEncode(encodeText))	
-	def decode(self, sourceText, charset = 'utf-8'):
-		decodeSourceText = self.cipherEncode(base64.b64decode(sourceText))		
-		textLength = len(decodeSourceText)
-		decodeText = ''
-		i = 0
-		while i < textLength:
-			decodeText = '{0}{1}'.format(decodeText, chr(ord(decodeSourceText[i]) ^ ord(decodeSourceText[i+1])))
-			i = i + 2
-		if charset != self.charset:
-			decodeText =  unicode(decodeText, charset).encode(self.charset)
-		return decodeText
-
-if __name__ == '__main__':  	
-	azdg = AzDG('guo')
-	#azdg = AzDG()
-	print azdg.getCipher()
-	m2 = azdg.encode('1234')
-	print m2
-	print azdg.decode(m2)
-
+############################################################  
+#                                                          #  
+# The implementation of PHPRPC Protocol 3.0                #  
+#                                                          #  
+# xxtea.py                                                 #  
+#                                                          #  
+# Release 3.0.0                                            #  
+# Copyright (c) 2005-2008 by Team-PHPRPC                   #  
+#                                                          #  
+# WebSite:  http://www.phprpc.org/                         #  
+#           http://www.phprpc.net/                         #  
+#           http://www.phprpc.com/                         #  
+#           http://sourceforge.net/projects/php-rpc/       #  
+#                                                          #  
+# Authors:  Ma Bingyao <andot@ujn.edu.cn>                  #  
+#                                                          #  
+# This file may be distributed and/or modified under the   #  
+# terms of the GNU Lesser General Public License (LGPL)    #  
+# version 3.0 as published by the Free Software Foundation #  
+# and appearing in the included file LICENSE.              #  
+#                                                          #  
+############################################################  
+#  
+# XXTEA encryption arithmetic library.  
+#  
+# Copyright (C) 2005-2008 Ma Bingyao <andot@ujn.edu.cn>  
+# Version: 1.0  
+# LastModified: Oct 5, 2008  
+# This library is free.  You can redistribute it and/or modify it.    
+import struct    
+_DELTA = 0x9E3779B9  
+  
+def _long2str(v, w):  
+    n = (len(v) - 1) << 2  
+    if w:  
+        m = v[-1]  
+        if (m < n - 3) or (m > n): return ''  
+        n = m  
+    s = struct.pack('<%iL' % len(v), *v)  
+    return s[0:n] if w else s  
+  
+def _str2long(s, w):  
+    n = len(s)  
+    m = (4 - (n & 3) & 3) + n  
+    s = s.ljust(m, "\0")  
+    v = list(struct.unpack('<%iL' % (m >> 2), s))  
+    if w: v.append(n)  
+    return v  
+  
+def encrypt(str, key):  
+    if str == '': return str  
+    v = _str2long(str, True)  
+    k = _str2long(key.ljust(16, "\0"), False)  
+    n = len(v) - 1  
+    z = v[n]  
+    y = v[0]  
+    sum = 0  
+    q = 6 + 52 // (n + 1)  
+    while q > 0:  
+        sum = (sum + _DELTA) & 0xffffffff  
+        e = sum >> 2 & 3  
+        for p in xrange(n):  
+            y = v[p + 1]  
+            v[p] = (v[p] + ((z >> 5 ^ y << 2) + (y >> 3 ^ z << 4) ^ (sum ^ y) + (k[p & 3 ^ e] ^ z))) & 0xffffffff  
+            z = v[p]  
+        y = v[0]  
+        v[n] = (v[n] + ((z >> 5 ^ y << 2) + (y >> 3 ^ z << 4) ^ (sum ^ y) + (k[n & 3 ^ e] ^ z))) & 0xffffffff  
+        z = v[n]  
+        q -= 1  
+    return _long2str(v, False)  
+  
+def decrypt(str, key):  
+    if str == '': return str  
+    v = _str2long(str, False)  
+    k = _str2long(key.ljust(16, "\0"), False)  
+    n = len(v) - 1  
+    z = v[n]  
+    y = v[0]  
+    q = 6 + 52 // (n + 1)  
+    sum = (q * _DELTA) & 0xffffffff  
+    while (sum != 0):  
+        e = sum >> 2 & 3  
+        for p in xrange(n, 0, -1):  
+            z = v[p - 1]  
+            v[p] = (v[p] - ((z >> 5 ^ y << 2) + (y >> 3 ^ z << 4) ^ (sum ^ y) + (k[p & 3 ^ e] ^ z))) & 0xffffffff  
+            y = v[p]  
+        z = v[n]  
+        v[0] = (v[0] - ((z >> 5 ^ y << 2) + (y >> 3 ^ z << 4) ^ (sum ^ y) + (k[0 & 3 ^ e] ^ z))) & 0xffffffff  
+        y = v[0]  
+        sum = (sum - _DELTA) & 0xffffffff  
+    return _long2str(v, True)  
+  
+if __name__ == "__main__":  
+    print decrypt(encrypt('Hello XXTEA!', '16bytelongstring'), '16bytelongstring') 
