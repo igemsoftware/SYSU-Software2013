38ca5c22fbbadc5da00e1074aac0ef460f2d308b
rathinho17 <rathinho17@gmail.com>
2013-08-30 01:10:27 +0800

    g

---
+++ project/Python27/Scripts/pyrsa-decrypt-script.py
@@ -0,0 +1,10 @@
+#!G:\igem2013_sysu_oschina\project\Python27\python2.7.exe
+# EASY-INSTALL-ENTRY-SCRIPT: 'rsa==3.1.1','console_scripts','pyrsa-decrypt'
+__requires__ = 'rsa==3.1.1'
+import sys
+from pkg_resources import load_entry_point
+
+if __name__ == '__main__':
+    sys.exit(
+        load_entry_point('rsa==3.1.1', 'console_scripts', 'pyrsa-decrypt')()
+    )
