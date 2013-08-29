1938a6a9d9382e3435a4007bd61e8c1a2a7e877b
guojiex <1036479561@qq.com>
2013-08-30 00:04:11 +0800

    修改生成公钥的代码

--- project/Python27/web/encrypt.py
+++ project/Python27/web/encrypt.py
@@ -1,20 +1,17 @@
 #coding=utf-8
 import rsa
-import string
-from random import choice
-'''generate random passwpord whose length is the param(length) default as 8''' 
-def generate_passwd(length=8,chars=string.ascii_letters+string.digits): 
-	return ''.join([choice(chars) for i in range(length)]) 
 
 class Encrypt:
 	def __init__ (self):
 		(pub_key, priv_key) = rsa.newkeys(256)
 		self.publicKey=pub_key
 		self.privateKey=priv_key
+	def getPublicKey(self):
+		return self.publicKey
 
 if __name__ == "__main__":     
-	(pub_key, priv_key) = rsa.key.newkeys(256)
-	print rsa.encrypt(generate_passwd(), pub_key)
+	(pub_key, priv_key) = rsa.key.newkeys(128)
+	print pub_key
 	#crypto =  rsa.encrypt('hello', pub_key)
 	#print rsa.decrypt(crypto, priv_key)
 	
