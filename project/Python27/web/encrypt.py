cbfcfc83592f78d92c95c00b722afc7aff05b0b0
guojiex <1036479561@qq.com>
2013-08-30 15:12:25 +0800

    实现了客户端用服务器端发送的公钥加密明文，然后服务器端解密的过程
    Signed-off-by: guojiex <1036479561@qq.com>

--- project/Python27/web/encrypt.py
+++ project/Python27/web/encrypt.py
@@ -1,5 +1,17 @@
 #coding=utf-8
 import rsa
+import os,sys
+import binascii
+base = [str(x) for x in range(10)] + [ chr(x) for x in range(ord('A'),ord('A')+6)]
+
+def dec2hex(string_num):
+	num = int(string_num)
+	mid = []
+	while True:
+		if num == 0: break
+		num,rem = divmod(num, 16)
+		mid.append(base[rem])
+	return ''.join([str(x) for x in mid[::-1]])
 
 class Encrypt:
 	def __init__ (self):
@@ -8,10 +20,13 @@ class Encrypt:
 		self.privateKey=priv_key
 	def getPublicKey(self):
 		return self.publicKey
-
+	def decrypt(self,crypto):
+		crypto = binascii.a2b_hex(crypto)
+		print crypto
+		return rsa.decrypt(crypto,self.privateKey)
 if __name__ == "__main__":     
 	(pub_key, priv_key) = rsa.key.newkeys(128)
-	print pub_key
+	print dec2hex(pub_key.n)
 	#crypto =  rsa.encrypt('hello', pub_key)
 	#print rsa.decrypt(crypto, priv_key)
 	
