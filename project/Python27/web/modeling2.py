19d97742806d79e368aa10ac73b42bcaa1a63ae0
omegaga <omegaleedh@gmail.com>
2013-09-05 15:29:51 +0800

    update database and modeling module

--- project/Python27/web/modeling2.py
+++ project/Python27/web/modeling2.py
@@ -55,7 +55,7 @@ def SteadyState_Concen(database, proteins, circuit, start_pos):
         promoter_type = circuit[n]["type"]
         Plasmid[n].SetData(promoter_type, proteins[n]["copy"], promoter['MPPromoter'], promoter['PoPS'], promoter['LeakageRate'], terminator['Efficiency'])
         ObjGene[n] = Struct2_SYSU_Software()
-        ObjGene[n].SetData(rbs['MPRBS'], objectgene['DegRatemRNA'], objectgene['DegRatePro'])
+        ObjGene[n].SetData(rbs['RIPS'], objectgene['DegRatemRNA'], objectgene['DegRatePro'])
         for i in circuit[n]["to"]:
             queue.append(i)
 
@@ -92,7 +92,7 @@ def SteadyState_Concen_ActRep(database, proteins, circuit, start_pos):
         promoter_type = circuit[n]["type"]
         Plasmid[n].SetData(promoter_type, proteins[n]["copy"], promoter['MPPromoter'], promoter['PoPS'], promoter['LeakageRate'], terminator['Efficiency'])
         ObjGene[n] = Struct2_SYSU_Software()
-        ObjGene[n].SetData(rbs1['MPRBS'], objectgene['DegRatemRNA'], objectgene['DegRatePro'])
+        ObjGene[n].SetData(rbs1['RIPS'], objectgene['DegRatemRNA'], objectgene['DegRatePro'])
         for i in circuit[n]["to"]:
             queue.append(i)
 
@@ -111,7 +111,7 @@ def SteadyState_Concen_ActRep(database, proteins, circuit, start_pos):
         regugene['DegRatemRNA'] = 0.00288
         regugene['DegRatePro']  = 0.00288
         ReguGene[n] = Struct3_SYSU_Software()
-        ReguGene[n].SetData(rbs2['MPRBS'], regugene['DegRatemRNA'], regugene['DegRatePro'], regugene['HillCoeff1'], regugene['K1'])
+        ReguGene[n].SetData(rbs2['RIPS'], regugene['DegRatemRNA'], regugene['DegRatePro'], regugene['HillCoeff1'], regugene['K1'])
     if Plasmid[start_pos].Type == 'Constitutive':
         ObjConcen [start_pos] = Plasmid[start_pos].CopyNumber * Plasmid[start_pos].TerEff * (Plasmid[start_pos].PoPS        / ObjGene [start_pos].DegRatemRNA) * (ObjGene [start_pos].RIPS / ObjGene [start_pos].DegRatePro)
         ReguConcen[start_pos] = Plasmid[start_pos].CopyNumber * Plasmid[start_pos].TerEff * (Plasmid[start_pos].PoPS        / ReguGene[start_pos].DegRatemRNA) * (ReguGene[start_pos].RIPS / ReguGene[start_pos].DegRatePro)
@@ -124,15 +124,11 @@ def SteadyState_Concen_ActRep(database, proteins, circuit, start_pos):
         for i in circuit[n]["to"]:
             queue.append(i)
 
-    queue = deque([start_pos])
+    queue = deque(circuit[start_pos]["to"])
     while len(queue) != 0:
         n = queue.popleft()
         cur_group  = circuit[n]["sbol"]
         last = circuit[n]["from"]
-        if last == -1:
-            for i in circuit[n]["to"]:
-                queue.append(i)
-            continue
         if Plasmid[n].Type == 'Positive':
             ObjConcen [n] = Plasmid[n].CopyNumber * Plasmid[n].TerEff * (((Plasmid[n].MPPromoter - Plasmid[n].LeakageRate) / (1 + pow(ReguGene[last].K1 / ReguConcen[last], ReguGene[last].HillCoeff1)) + Plasmid[n].LeakageRate) / ObjGene [n].DegRatemRNA) * (ObjGene [n].RIPS / ObjGene [n].DegRatePro)
             if circuit[n]["to"] == []:
@@ -143,61 +139,95 @@ def SteadyState_Concen_ActRep(database, proteins, circuit, start_pos):
             if circuit[n]["to"] == []:
                 continue
             ReguConcen[n] = Plasmid[n].CopyNumber * Plasmid[n].TerEff * (((Plasmid[n].MPPromoter - Plasmid[n].LeakageRate) / (1 + pow(ReguConcen[last] / ReguGene[last].K1, ReguGene[last].HillCoeff1)) + Plasmid[n].LeakageRate) / ReguGene[n].DegRatemRNA) * (ReguGene[n].RIPS /ReguGene[n].DegRatePro)
+        for i in circuit[n]["to"]:
+            queue.append(i)
     return ObjConcen
 
-def SteadyState_Concen_CorepInd(database, copynumber, circuit, concen, corepind):
+def SteadyState_Concen_CorepInd(database, proteins, circuit, corepind, start_pos):
     # circuit: [Promoter, RBS1, ObjectGene, RBS2/None, RegulationGene(Activator/Repressor/None), Terminator] * size
     # concen: concentration of co-repressor or inducer
     # corepind: co-repressor or inducer, corepind[0] = None for circuit[0] is express without regulation
     size = len(circuit)
-    Plasmid    = [None] * size
-    ObjGene    = [None] * size
-    ReguGene   = [None] * (size - 1)
-    ObjConcen  = [None] * size
-    ReguConcen = [None] * (size - 1)
-    for n in range(size):
-        promoter   = database.select_with_name('Promoter', circuit[n][0]['Name'])
-        rbs1       = database.select_with_name('RBS', circuit[n][1]['Name'])
+    Plasmid    = {}
+    ObjGene    = {}
+    ReguGene   = {}
+    ObjConcen  = {}
+    ReguConcen = {}
+    queue = deque([start_pos])
+    while len(queue) != 0:
+        n = queue.popleft()
+        cur_group  = circuit[n]["sbol"]
+        promoter   = database.select_with_name('Promoter', cur_group[0]['name'])
+        rbs1       = database.select_with_name('RBS', cur_group[1]['name'])
         objectgene = {'DegRatemRNA': 0.00288, 'DegRatePro': 0.00288}
-        terminator = database.select_with_naem('Terminator', circuit[n][-1]['Name'])
+        terminator = database.select_with_name('Terminator', cur_group[-1]['name'])
         Plasmid[n] = Struct1_SYSU_Software()
-        Plasmid[n].SetData(promoter['Type'], copynumber[n], promoter['MPPromoter'], promoter['PoPS'], promoter['LeakageRate'], terminator['TerEff'])
+        promoter_type = circuit[n]["type"]
+        copynumber = proteins[n]["copy"]
+        Plasmid[n].SetData(promoter_type, copynumber, promoter['MPPromoter'], promoter['PoPS'], promoter['LeakageRate'], terminator['Efficiency'])
         ObjGene[n] = Struct2_SYSU_Software()
         ObjGene[n].SetData(rbs1['RIPS'], objectgene['DegRatemRNA'], objectgene['DegRatePro'])
-    for n in range(size-1):
-        rbs2       = database.select_with_name('RBS', circuit[n][3]['Name'])
-        if Plasmid[n+1].Type == 'Positive':
-            regugene = database.select_with_name('Activator', circuit[n][4]['Name'])
-        elif Plasmid[n+1].Type == 'Negative':
-            regugene = database.select_with_name('Repressor', circuit[n][4]['Name'])
-        regugene['DegRatemRNA'] = 0.00288; regugene['DegRatePro'] = 0.00288
+        for i in circuit[n]["to"]:
+            queue.append(i)
+
+    queue = deque([start_pos])
+    while len(queue) != 0:
+        n = queue.popleft()
+        cur_group  = circuit[n]["sbol"]
+        rbs2       = database.select_with_name('RBS', cur_group[3]['name'])
+        if circuit[n]["to"] == []:
+            continue
+        next_type  = circuit[circuit[n]["to"][0]]["type"]
+        if next_type == 'Positive':
+            regugene = database.select_with_name('Activator', cur_group[4]['name'])
+        elif next_type == 'Negative':
+            regugene = database.select_with_name('Repressor', cur_group[4]['name'])
+        regugene['DegRatemRNA'] = 0.00288
+        regugene['DegRatePro'] = 0.00288
         ReguGene[n] = Struct3_SYSU_Software()
         ReguGene[n].SetData(rbs2['RIPS'], regugene['DegRatemRNA'], regugene['DegRatePro'], regugene['HillCoeff1'], regugene['K1'])
-    CorepIndConst    = [None] * size
-    for n in range(1, size):
-        if corepind != None:
-            corepind = database.select_with_name('Corepressor_Inducer', corepind[n]['Name'])
-            CorepIndConst[n] = 1 + (concen[n] / corepind[n]['K2']) ** corepind[n]['HillCoeff2']
+        for i in circuit[n]["to"]:
+            queue.append(i)
+
+    CorepIndConst    = {}
+    queue = deque(circuit[start_pos]["to"])
+    while len(queue) != 0:
+        n = queue.popleft()
+        if corepind != {}:
+            corepind = database.select_with_name('Corepressor_Inducer', corepind[n]['name'])
+            CorepIndConst[n] = 1 + (proteins[n]["concen"] / corepind[n]['K2']) ** corepind[n]['HillCoeff2']
         else:
             CorepIndConst[n] = 1.0
-    if Plasmid[0].Type == 'Constitutive':
-        ObjConcen [0] = Plasmid[0].CopyNumber * Plasmid[0].TerEff * (Plasmid[0].PoPS        / ObjGene [0].DegRatemRNA) * (ObjGene [0].RIPS / ObjGene [0].DegRatePro)
-        ReguConcen[0] = Plasmid[0].CopyNumber * Plasmid[0].TerEff * (Plasmid[0].PoPS        / ReguGene[0].DegRatemRNA) * (ReguGene[0].RIPS / ReguGene[0].DegRatePro)
-    elif Plasmid[0].Type == 'Positive':
-        ObjConcen [0] = Plasmid[0].CopyNumber * Plasmid[0].TerEff * (Plasmid[0].LeakageRate / ObjGene [0].DegRatemRNA) * (ObjGene [0].RIPS / ObjGene [0].DegRatePro)
-        ReguConcen[0] = Plasmid[0].CopyNumber * Plasmid[0].TerEff * (Plasmid[0].LeakageRate / ReguGene[0].DegRatemRNA) * (ReguGene[0].RIPS / ReguGene[0].DegRatePro)
-    elif Plasmid[0].Type == 'Negative':
-        ObjConcen [0] = Plasmid[0].CopyNumber * Plasmid[0].TerEff * (Plasmid[0].MPPromoter  / ObjGene [0].DegRatemRNA) * (ObjGene [0].RIPS / ObjGene [0].DegRatePro)
-        ReguConcen[0] = Plasmid[0].CopyNumber * Plasmid[0].TerEff * (Plasmid[0].MPPromoter  / ReguGene[0].DegRatemRNA) * (ReguGene[0].RIPS / ReguGene[0].DegRatePro)
-    for n in range(1, size):
+        for i in circuit[n]["to"]:
+            queue.append(i)
+
+    if Plasmid[start_pos].Type == 'Constitutive':
+        ObjConcen [start_pos] = Plasmid[start_pos].CopyNumber * Plasmid[start_pos].TerEff * (Plasmid[start_pos].PoPS        / ObjGene [start_pos].DegRatemRNA) * (ObjGene [start_pos].RIPS / ObjGene [start_pos].DegRatePro)
+        ReguConcen[start_pos] = Plasmid[start_pos].CopyNumber * Plasmid[start_pos].TerEff * (Plasmid[start_pos].PoPS        / ReguGene[start_pos].DegRatemRNA) * (ReguGene[start_pos].RIPS / ReguGene[start_pos].DegRatePro)
+    elif Plasmid[start_pos].Type == 'Positive':
+        ObjConcen [start_pos] = Plasmid[start_pos].CopyNumber * Plasmid[start_pos].TerEff * (Plasmid[start_pos].LeakageRate / ObjGene [start_pos].DegRatemRNA) * (ObjGene [start_pos].RIPS / ObjGene [start_pos].DegRatePro)
+        ReguConcen[start_pos] = Plasmid[start_pos].CopyNumber * Plasmid[start_pos].TerEff * (Plasmid[start_pos].LeakageRate / ReguGene[start_pos].DegRatemRNA) * (ReguGene[start_pos].RIPS / ReguGene[start_pos].DegRatePro)
+    elif Plasmid[start_pos].Type == 'Negative':
+        ObjConcen [start_pos] = Plasmid[start_pos].CopyNumber * Plasmid[start_pos].TerEff * (Plasmid[start_pos].MPPromoter  / ObjGene [start_pos].DegRatemRNA) * (ObjGene [start_pos].RIPS / ObjGene [start_pos].DegRatePro)
+        ReguConcen[start_pos] = Plasmid[start_pos].CopyNumber * Plasmid[start_pos].TerEff * (Plasmid[start_pos].MPPromoter  / ReguGene[start_pos].DegRatemRNA) * (ReguGene[start_pos].RIPS / ReguGene[start_pos].DegRatePro)
+
+    queue = deque(circuit[start_pos]["to"])
+    while len(queue) != 0:
+        n = queue.popleft()
+        cur_group  = circuit[n]["sbol"]
+        last = circuit[n]["from"]
         if Plasmid[n].Type == 'Positive':
-            ObjConcen [n] = Plasmid[n].CopyNumber * Plasmid[n].TerEff * (((Plasmid[n].MPPromoter - Plasmid[n].LeakageRate) / (1 + pow(ReguGene[n-1].K1 * CorepIndConst[n] / ReguConcen[n-1], ReguGene[n-1].HillCoeff1)) + Plasmid[n].LeakageRate) / ObjGene [n].DegRatemRNA) * (ObjGene [n].RIPS / ObjGene [n].DegRatePro)
-            if n == size-1: continue
-            ReguConcen[n] = Plasmid[n].CopyNumber * Plasmid[n].TerEff * (((Plasmid[n].MPPromoter - Plasmid[n].LeakageRate) / (1 + pow(ReguGene[n-1].K1 * CorepIndConst[n] / ReguConcen[n-1], ReguGene[n-1].HillCoeff1)) + Plasmid[n].LeakageRate) / ReguGene[n].DegRatemRNA) * (ReguGene[n].RIPS / ReguGene[n].DegRatePro)
+            ObjConcen [n] = Plasmid[n].CopyNumber * Plasmid[n].TerEff * (((Plasmid[n].MPPromoter - Plasmid[n].LeakageRate) / (1 + pow(ReguGene[last].K1 * CorepIndConst[n] / ReguConcen[last], ReguGene[last].HillCoeff1)) + Plasmid[n].LeakageRate) / ObjGene [n].DegRatemRNA) * (ObjGene [n].RIPS / ObjGene [n].DegRatePro)
+            if circuit[n]["to"] == []:
+                continue
+            ReguConcen[n] = Plasmid[n].CopyNumber * Plasmid[n].TerEff * (((Plasmid[n].MPPromoter - Plasmid[n].LeakageRate) / (1 + pow(ReguGene[last].K1 * CorepIndConst[n] / ReguConcen[last], ReguGene[last].HillCoeff1)) + Plasmid[n].LeakageRate) / ReguGene[n].DegRatemRNA) * (ReguGene[n].RIPS / ReguGene[n].DegRatePro)
         elif Plasmid[n].Type == 'Negative':
-            ObjConcen [n] = Plasmid[n].CopyNumber * Plasmid[n].TerEff * (((Plasmid[n].MPPromoter - Plasmid[n].LeakageRate) / (1 + pow(ReguConcen[n-1] / ReguGene[n-1].K1 / CorepIndConst[n], ReguGene[n-1].HillCoeff1)) + Plasmid[n].LeakageRate) / ObjGene [n].DegRatemRNA) * (ObjGene [n].RIPS / ObjGene [n].DegRatePro)
-            if n == size-1: continue
-            ReguConcen[n] = Plasmid[n].CopyNumber * Plasmid[n].TerEff * (((Plasmid[n].MPPromoter - Plasmid[n].LeakageRate) / (1 + pow(ReguConcen[n-1] / ReguGene[n-1].K1 / CorepIndConst[n], ReguGene[n-1].HillCoeff1)) + Plasmid[n].LeakageRate) / ReguGene[n].DegRatemRNA) * (ReguGene[n].RIPS / ReguGene[n].DegRatePro)
+            ObjConcen [n] = Plasmid[n].CopyNumber * Plasmid[n].TerEff * (((Plasmid[n].MPPromoter - Plasmid[n].LeakageRate) / (1 + pow(ReguConcen[last] / ReguGene[last].K1 / CorepIndConst[n], ReguGene[last].HillCoeff1)) + Plasmid[n].LeakageRate) / ObjGene [n].DegRatemRNA) * (ObjGene [n].RIPS / ObjGene [n].DegRatePro)
+            if circuit[n]["to"] == []:
+                continue
+            ReguConcen[n] = Plasmid[n].CopyNumber * Plasmid[n].TerEff * (((Plasmid[n].MPPromoter - Plasmid[n].LeakageRate) / (1 + pow(ReguConcen[last] / ReguGene[last].K1 / CorepIndConst[n], ReguGene[last].HillCoeff1)) + Plasmid[n].LeakageRate) / ReguGene[n].DegRatemRNA) * (ReguGene[n].RIPS / ReguGene[n].DegRatePro)
+        for i in circuit[n]["to"]:
+            queue.append(i)
     return ObjConcen
 
 
@@ -233,4 +263,4 @@ if __name__ == "__main__":
           383.20873499999993, 'grp_id': 4, 'PoPs': 55.55, 'repress_rate':
           0.2998365890589771, 'induce_rate': 0, 'after_induced': 0, 'copy':
           3.0, 'after_regulated': 0}}
-  print SteadyState_Concen_ActRep(db, proteins, circuit, 2)
+  print SteadyState_Concen_CorepInd(db, proteins, circuit, {}, 2)
