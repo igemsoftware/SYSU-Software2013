afb6a1dc120f2b517b2613e8220dd9ff97bd53bf
omegaga <omegaleedh@gmail.com>
2013-09-13 14:35:36 +0800

    add plasmid backbone into plasmid sbol

--- project/Python27/web/plasmid.py
+++ project/Python27/web/plasmid.py
@@ -1,104 +1,108 @@
 import component_union
 import sequence_serializer
 import os
+import database
 
-groups = [
-    {
+groups = {
+    "copy": 23.00,
+    "circuit":[
+      {
         "sbol": [
-            {
-                "type": "Signalling",
-                "name": "BBa_K266005"
+          {
+            "type": "Signalling",
+            "name": "BBa_K266005"
             },
-            {
-                "type": "RBS",
-                "name": "BBa_J61104"
+          {
+            "type": "RBS",
+            "name": "BBa_J61104"
             },
-            {
-                "type": "Coding",
-                "name": "BBa_C0060"
+          {
+            "type": "Coding",
+            "name": "BBa_C0060"
             },
-            {
-                "type": "RBS",
-                "name": "BBa_J61104"
+          {
+            "type": "RBS",
+            "name": "BBa_J61104"
             },
-            {
-                "type": "Coding",
-                "name": "BBa_C0060"
+          {
+            "type": "Coding",
+            "name": "BBa_C0060"
             },
-            {
-                "type": "RBS",
-                "name": "BBa_J61104"
+          {
+            "type": "RBS",
+            "name": "BBa_J61104"
             },
-            {
-                "type": "Coding",
-                "name": "BBa_K518003"
+          {
+            "type": "Coding",
+            "name": "BBa_K518003"
             },
-            {
-                "type": "RBS",
-                "name": "BBa_J61104"
+          {
+            "type": "RBS",
+            "name": "BBa_J61104"
             },
-            {
-                "type": "Coding",
-                "name": "BBa_K518003"
+          {
+            "type": "Coding",
+            "name": "BBa_K518003"
             },
-            {
-                "type": "Terminator",
-                "name": "BBa_B0013"
+          {
+            "type": "Terminator",
+            "name": "BBa_B0013"
             }
-        ],
+          ],
         "state": "cis"
-    },
-    {
+        },
+      {
         "sbol": [
-            {
-                "type": "Regulatory",
-                "name": "BBa_I712074"
+          {
+            "type": "Regulatory",
+            "name": "BBa_I712074"
             },
-            {
-                "type": "RBS",
-                "name": "BBa_J61104"
+          {
+            "type": "RBS",
+            "name": "BBa_J61104"
             },
-            {
-                "type": "Coding",
-                "name": "BBa_C0160"
+          {
+            "type": "Coding",
+            "name": "BBa_C0160"
             },
-            {
-                "type": "Terminator",
-                "name": "BBa_B0013"
+          {
+            "type": "Terminator",
+            "name": "BBa_B0013"
             }
-        ],
+          ],
         "state": "cis"
-    },
-    {
-        "sbol": [
+        },
+      {
+          "sbol": [
             {
-                "type": "Regulatory",
-                "name": "BBa_I712074"
-            },
+              "type": "Regulatory",
+              "name": "BBa_I712074"
+              },
             {
-                "type": "RBS",
-                "name": "BBa_J61104"
-            },
+              "type": "RBS",
+              "name": "BBa_J61104"
+              },
             {
-                "type": "Coding",
-                "name": "BBa_C0178"
-            },
+              "type": "Coding",
+              "name": "BBa_C0178"
+              },
             {
-                "type": "RBS",
-                "name": "BBa_J61104"
-            },
+              "type": "RBS",
+              "name": "BBa_J61104"
+              },
             {
-                "type": "Coding",
-                "name": "BBa_C0178"
-            },
+              "type": "Coding",
+              "name": "BBa_C0178"
+              },
             {
-                "type": "Terminator",
-                "name": "BBa_B0013"
-            }
-        ],
-        "state": "cis"
+              "type": "Terminator",
+              "name": "BBa_B0013"
+              }
+            ],
+          "state": "cis"
+          }
+      ]
     }
-]
 
 reverse = {"A":"T", "T":"A", "C":"G", "G":"C", "a":"t", "t":"a", "g":"c","c":"g"}
 
@@ -111,13 +115,18 @@ def trans(s):
   ret = ''.join([reverse[i] for i in s[::-1]])
   return ret
 
-def plasmid_sbol(groups, rule = "RFC10"):
-  ret = []
+def plasmid_sbol(db, groups, rule = "RFC10"):
+  circuit = groups["circuit"]
+  copy = groups["copy"]
+  components = []
   sequence = ""
   offset = 0
-  for data in groups:
-    components = [cc["name"] for cc in data["sbol"]]
-    file_list = [find_file(s + ".xml", ".") for s in components]
+  plasmid_backbone = db.getPlasmidBackboneNearValue(copy)["Number"]
+  circuit[-1]["sbol"].append({"type": "PlasmidBackbone", "name":
+    plasmid_backbone})
+  for data in circuit:
+    comp_names = [cc["name"] for cc in data["sbol"]]
+    file_list = [find_file(s + ".xml", ".") for s in comp_names]
     content = component_union.union(rule, file_list)
     dna_sequence = component_union.connect(rule, content)
     sbol = component_union.formatter_v11(content, dna_sequence)
@@ -127,14 +136,15 @@ def plasmid_sbol(groups, rule = "RFC10"):
       tmp["bioStart"] = str(int(tmp["bioStart"]) + offset)
       tmp["bioEnd"] = str(int(tmp["bioEnd"]) + offset)
     offset += len(sbol2["DnaComponent"]["DnaSequence"]["nucleotides"])
-    ret += sbol2["DnaComponent"]["annotations"]
+    components += sbol2["DnaComponent"]["annotations"]
     if data["state"] == "trans":
       sequence += trans(sbol2["DnaComponent"]["DnaSequence"]["nucleotides"])
     else:
       sequence += sbol2["DnaComponent"]["DnaSequence"]["nucleotides"]
-  sbol2["DnaComponent"]["annotations"] = ret
+  sbol2["DnaComponent"]["annotations"] = components
   sbol2["DnaComponent"]["DnaSequence"]["nucleotides"] = sequence
   return sbol2
 
 if __name__ == "__main__":
-  print plasmid_sbol(groups)
+  db = database.SqliteDatabase()
+  print plasmid_sbol(db, groups)
