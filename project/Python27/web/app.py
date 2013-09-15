588957befd3e8d6c634c3b1df81c2184dbfb9963
guojiex <1036479561@qq.com>
2013-09-15 20:54:36 +0800

    fix bug in app.py
    Signed-off-by: guojiex <1036479561@qq.com>

--- project/Python27/web/app.py
+++ project/Python27/web/app.py
@@ -22,7 +22,7 @@ def login():
 		return render_template('login.html')
 		
 @app.route("/register")
-def login():	
+def register():	
 	return render_template('register.html')
 	
 @app.route("/index")
