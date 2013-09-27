236795a1a70960fb679a3725d174faa970385ace
omegaga <omegaleedh@gmail.com>
2013-09-27 23:33:03 +0800

    debug

--- project/Python27/web/Simulate_Function.py
+++ project/Python27/web/Simulate_Function.py
@@ -128,12 +128,14 @@ def Simulate(isStochastic, circuit, corepind, database, time, dt):
           data[i] = [float('%0.3f'%x) for x in data[i]]
         ret['data'] = data
         return ret
-    except InvalidParameter:
+    except InvalidParameter as e:
+        print e
         return 'Invalid Paramter!'
-    except IllegalSetting:
+    except IllegalSetting as e:
+        print e
         return 'Illegal Setting!'
-    #except Exception as e:
-        #return 'Something Unexpected Happened!'
+    except Exception as e:
+        return 'Something Unexpected Happened!'
 
 if __name__ == "__main__":
 
@@ -141,6 +143,9 @@ if __name__ == "__main__":
     db = database.SqliteDatabase()
     #corepind = {5: {"time": 20},
     #            7: {"time": 60}}
-    corepind = {u'64bf4265-e3ac-f176-6fd0-b7eb11282ad6': {u'time': 1484.3982169390788}}
-    gene_circuit = {"proteins":{"c34ddc9d-d4a6-d308-501a-1fa03c6c056f":{"RiPS":0.336,"name":"BBa_C0082","before_regulated":20.60352,"concen":None,"grp_id":"cb9c971a-c48f-1068-a59c-650080baf8a5","pos":2,"PoPS":0.84,"repress_rate":0,"K1":None,"induce_rate":0,"copy":73,"display":True},"cb9c971a-c48f-1068-a59c-650080baf8a5":{"RiPS":0.336,"name":"BBa_C0040","before_regulated":20.60352,"concen":None,"grp_id":"cb9c971a-c48f-1068-a59c-650080baf8a5","pos":4,"PoPS":0.84,"repress_rate":0,"K1":None,"induce_rate":0,"copy":73,"display":False},"64bf4265-e3ac-f176-6fd0-b7eb11282ad6":{"RiPS":0.336,"name":"BBa_I15010","before_regulated":11.969664,"concen":0.1,"grp_id":"64bf4265-e3ac-f176-6fd0-b7eb11282ad6","pos":2,"PoPS":0.488,"repress_rate":0,"K1":0.6434526764861874,"induce_rate":0,"copy":73,"display":True}},"plasmids":[["cb9c971a-c48f-1068-a59c-650080baf8a5","64bf4265-e3ac-f176-6fd0-b7eb11282ad6"]],"groups":{"cb9c971a-c48f-1068-a59c-650080baf8a5":{"from":-1,"state":"cis","corep_ind_type":"None","to":["64bf4265-e3ac-f176-6fd0-b7eb11282ad6"],"sbol":[{"type":"Promoter","name":"BBa_I712074"},{"type":"RBS","name":"BBa_J61104"},{"type":"Protein","name":"BBa_C0082","id":"c34ddc9d-d4a6-d308-501a-1fa03c6c056f"},{"type":"RBS","name":"BBa_J61104"},{"type":"Repressor","name":"BBa_C0040","id":"cb9c971a-c48f-1068-a59c-650080baf8a5"},{"type":"Terminator","name":"BBa_B0013"}],"type":"Constitutive"},"64bf4265-e3ac-f176-6fd0-b7eb11282ad6":{"from":"cb9c971a-c48f-1068-a59c-650080baf8a5","state":"cis","corep_ind_type":"Inducer","to":[],"sbol":[{"type":"Promoter","name":"BBa_I12212"},{"type":"RBS","name":"BBa_J61104"},{"type":"Protein","name":"BBa_I15010","id":"64bf4265-e3ac-f176-6fd0-b7eb11282ad6"},{"type":"Terminator","name":"BBa_B0013"}],"type":"Negative"}}}
+    corepind = {}
+    gene_circuit = u'{"proteins":{"0e72fd0d-9d44-ce43-63f2-9cfb90493d35":{"RiPS":0.336,"name":"BBa_K112004","repress_rate":2.8750612545823317,"concen":null,"grp_id":"0e72fd0d-9d44-ce43-63f2-9cfb90493d35","pos":2,"PoPS":0.0002,"before_regulated":0.0049056,"K1":-2,"induce_rate":2.8750612545823317,"copy":73,"display":true},"1bc44a5b-4940-6e01-3b0b-5550a154e0ef":{"RiPS":0.336,"name":"BBa_K108001","repress_rate":0,"concen":null,"grp_id":"4ea05319-2f8f-ef3a-43f2-627f408f365b","pos":2,"PoPS":0.84,"before_regulated":20.60352,"K1":null,"induce_rate":0,"copy":73,"display":true},"4ea05319-2f8f-ef3a-43f2-627f408f365b":{"RiPS":0.336,"name":"BBa_C0077","repress_rate":0,"concen":null,"grp_id":"4ea05319-2f8f-ef3a-43f2-627f408f365b","pos":4,"PoPS":0.84,"before_regulated":20.60352,"K1":null,"induce_rate":0,"copy":73,"display":false}},"plasmids":[["0e72fd0d-9d44-ce43-63f2-9cfb90493d35","4ea05319-2f8f-ef3a-43f2-627f408f365b"]],"groups":{"0e72fd0d-9d44-ce43-63f2-9cfb90493d35":{"from":"4ea05319-2f8f-ef3a-43f2-627f408f365b","state":"cis","corep_ind_type":"None","to":[],"sbol":[{"type":"Promoter","name":"BBa_R0078"},{"type":"RBS","name":"BBa_J61104"},{"type":"Protein","name":"BBa_K112004","id":"0e72fd0d-9d44-ce43-63f2-9cfb90493d35"},{"type":"Terminator","name":"BBa_B0013"}],"type":"Positive","id":"0e72fd0d-9d44-ce43-63f2-9cfb90493d35"},"4ea05319-2f8f-ef3a-43f2-627f408f365b":{"from":-1,"state":"cis","corep_ind_type":"None","to":["0e72fd0d-9d44-ce43-63f2-9cfb90493d35"],"sbol":[{"type":"Promoter","name":"BBa_I712074"},{"type":"RBS","name":"BBa_J61104"},{"type":"Protein","name":"BBa_K108001","id":"1bc44a5b-4940-6e01-3b0b-5550a154e0ef"},{"type":"RBS","name":"BBa_J61104"},{"type":"Activator","name":"BBa_C0077","id":"4ea05319-2f8f-ef3a-43f2-627f408f365b"},{"type":"Terminator","name":"BBa_B0013"}],"type":"Constitutive","id":"4ea05319-2f8f-ef3a-43f2-627f408f365b"}}}'
+    import json
+    gene_circuit = json.loads(gene_circuit)
+    print gene_circuit
     print Simulate(False, gene_circuit, corepind, db, 6000, 100)
