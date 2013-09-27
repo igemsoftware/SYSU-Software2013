ae4b80be592d33bc0ae1506a51f2f5b143fb450c
omegaga <omegaleedh@gmail.com>
2013-09-28 00:05:30 +0800

    unnecessary exception handling

--- project/Python27/web/Simulate_Function.py
+++ project/Python27/web/Simulate_Function.py
@@ -128,14 +128,14 @@ def Simulate(isStochastic, circuit, corepind, database, time, dt):
           data[i] = [float('%0.3f'%x) for x in data[i]]
         ret['data'] = data
         return ret
-    except InvalidParameter as e:
-        print e
-        return 'Invalid Paramter!'
+    #except InvalidParameter as e:
+        #print e
+        #return 'Invalid Paramter!'
     except IllegalSetting as e:
         print e
         return 'Illegal Setting!'
-    except Exception as e:
-        return 'Something Unexpected Happened!'
+    #except Exception as e:
+        #return 'Something Unexpected Happened!'
 
 if __name__ == "__main__":
 
@@ -147,5 +147,4 @@ if __name__ == "__main__":
     gene_circuit = u'{"proteins":{"0e72fd0d-9d44-ce43-63f2-9cfb90493d35":{"RiPS":0.336,"name":"BBa_K112004","repress_rate":2.8750612545823317,"concen":null,"grp_id":"0e72fd0d-9d44-ce43-63f2-9cfb90493d35","pos":2,"PoPS":0.0002,"before_regulated":0.0049056,"K1":-2,"induce_rate":2.8750612545823317,"copy":73,"display":true},"1bc44a5b-4940-6e01-3b0b-5550a154e0ef":{"RiPS":0.336,"name":"BBa_K108001","repress_rate":0,"concen":null,"grp_id":"4ea05319-2f8f-ef3a-43f2-627f408f365b","pos":2,"PoPS":0.84,"before_regulated":20.60352,"K1":null,"induce_rate":0,"copy":73,"display":true},"4ea05319-2f8f-ef3a-43f2-627f408f365b":{"RiPS":0.336,"name":"BBa_C0077","repress_rate":0,"concen":null,"grp_id":"4ea05319-2f8f-ef3a-43f2-627f408f365b","pos":4,"PoPS":0.84,"before_regulated":20.60352,"K1":null,"induce_rate":0,"copy":73,"display":false}},"plasmids":[["0e72fd0d-9d44-ce43-63f2-9cfb90493d35","4ea05319-2f8f-ef3a-43f2-627f408f365b"]],"groups":{"0e72fd0d-9d44-ce43-63f2-9cfb90493d35":{"from":"4ea05319-2f8f-ef3a-43f2-627f408f365b","state":"cis","corep_ind_type":"None","to":[],"sbol":[{"type":"Promoter","name":"BBa_R0078"},{"type":"RBS","name":"BBa_J61104"},{"type":"Protein","name":"BBa_K112004","id":"0e72fd0d-9d44-ce43-63f2-9cfb90493d35"},{"type":"Terminator","name":"BBa_B0013"}],"type":"Positive","id":"0e72fd0d-9d44-ce43-63f2-9cfb90493d35"},"4ea05319-2f8f-ef3a-43f2-627f408f365b":{"from":-1,"state":"cis","corep_ind_type":"None","to":["0e72fd0d-9d44-ce43-63f2-9cfb90493d35"],"sbol":[{"type":"Promoter","name":"BBa_I712074"},{"type":"RBS","name":"BBa_J61104"},{"type":"Protein","name":"BBa_K108001","id":"1bc44a5b-4940-6e01-3b0b-5550a154e0ef"},{"type":"RBS","name":"BBa_J61104"},{"type":"Activator","name":"BBa_C0077","id":"4ea05319-2f8f-ef3a-43f2-627f408f365b"},{"type":"Terminator","name":"BBa_B0013"}],"type":"Constitutive","id":"4ea05319-2f8f-ef3a-43f2-627f408f365b"}}}'
     import json
     gene_circuit = json.loads(gene_circuit)
-    print gene_circuit
     print Simulate(False, gene_circuit, corepind, db, 6000, 100)
