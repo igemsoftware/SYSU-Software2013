43ec30d19227fb17379851bfb30183488b85f9d1
guojiex <1036479561@qq.com>
2013-10-27 00:34:27 +0800

    Signed-off-by: guojiex <1036479561@qq.com>

--- project/Python27/web/static/js/plasmid (2).js
+++ project/Python27/web/static/js/plasmid (2).js
@@ -760,6 +760,12 @@ function isPointInCircle(circle,x,y)
     return false;
 } 
 $(function(){	
+	$('#linknext').click(function(){
+		window.location.pathname = "/simulation";
+	});
+	$('#linkabove').click(function(){
+		window.location.pathname = "/protocol";
+	});
 	handlerWebSocket();		
 });
 function drawThePlasmid()
