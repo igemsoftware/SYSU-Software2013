9115b6c921550b1d0ef39ed5233ce0024e27f889
whyuan <519316166@qq.com>
2013-10-26 22:43:01 +0800

    update genecircuit: navigation

--- project/Python27/web/static/js/genecircuit/genecircuit.js
+++ project/Python27/web/static/js/genecircuit/genecircuit.js
@@ -972,11 +972,12 @@ $(function(){
 		plasmid.addPlasmid();
 	});
 
-	$("#index").click(function(){
+	$("#linknext").click(function(){
 		$(".view-plasmid").tooltip('show');
-		$("#btn-edit").tooltip('show');
+		/* $("#btn-edit").tooltip('show'); */
 		$("#btn-viewmore").tooltip('show');
-		setTimeout("$(\".view-plasmid\").tooltip('hide');$(\"#btn-edit\").tooltip('hide');$(\"#btn-viewmore\").tooltip('hide');", 500);
+		/* setTimeout("$(\".view-plasmid\").tooltip('hide');$(\"#btn-edit\").tooltip('hide');$(\"#btn-viewmore\").tooltip('hide');", 500); */
+		setTimeout("$(\".view-plasmid\").tooltip('hide');$(\"#btn-viewmore\").tooltip('hide');", 500);
 	});
 });
 
