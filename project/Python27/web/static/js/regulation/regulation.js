aaf0c5606724fb52650e725a495f087435cc1b45
rathinho <rathinho17@gmail.com>
2013-10-26 17:06:53 +0800

    replace pictures
    Signed-off-by: rathinho <rathinho17@gmail.com>

--- project/Python27/web/static/js/regulation/regulation.js
+++ project/Python27/web/static/js/regulation/regulation.js
@@ -404,7 +404,6 @@ $().ready(function() {
     // Cleanly close websocket when unload window
     window.onbeforeunload = function() {
         var jsonData = JSON.stringify(canvasToJSON());
-        // console.log(jsonData);
         ws.send(JSON.stringify({
             'request': 'indexSaveToGeneCircuit',
             'data': jsonData
